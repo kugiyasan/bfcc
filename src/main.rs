146246aaaca158ebe646aa9a76ede26dbc674697
kugiyasan <kugiyasan@users.noreply.github.com>
2023-08-24 20:54:37 -0400

    return code works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// https://www.sigbus.info/compilerbook
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let env: Vec<String> = env::args_os().map(|s| s.into_string().unwrap()).collect();
+    if env.len() != 2 {
+        eprintln!("Wrong number of arguments!");
+        return;
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", env[1].parse::<u8>().unwrap());
+    println!("  ret");
 }
