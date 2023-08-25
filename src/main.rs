664eb4ec2499a3e21bb75571f69195632bf941a1
kugiyasan <kugiyasan@users.noreply.github.com>
2023-08-24 23:57:26 -0400

    can add and sub return code

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,31 @@ fn main() {
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!("  mov rax, {}", env[1].parse::<u8>().unwrap());
+    let chars: Vec<_> = env[1].chars().collect();
+    let mut p = 0;
+    let n: String = chars.iter().take_while(|c| c.is_numeric()).collect();
+    println!("mov rax, {}", n.parse::<i32>().unwrap());
+    p += n.len();
+
+    while p < chars.len() {
+        let c = chars[p];
+        match c {
+            '+' => {
+                let n: String = chars[p+1..].iter().take_while(|c| c.is_numeric()).collect();
+                println!("add rax, {}", n.parse::<i32>().unwrap());
+                p += n.len() + 1;
+            }
+            '-' => {
+                let n: String = chars[p+1..].iter().take_while(|c| c.is_numeric()).collect();
+                println!("sub rax, {}", n.parse::<i32>().unwrap());
+                p += n.len() + 1;
+            }
+            _ => {
+                eprintln!("unexpected character: {}", c);
+                return;
+            }
+        }
+    }
+
     println!("  ret");
 }
